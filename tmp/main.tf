ac8f1c7a1170d676e42d03154e94eafb5af8c5b5
vjkancherla <61050114+vjkancherla@users.noreply.github.com>
2023-11-29 16:35:51 +0000

    Update main.tf

--- tmp/main.tf
+++ tmp/main.tf
@@ -1,73 +1,32 @@
-provider "aws" {
-  region = "us-west-2" # Replace with your desired AWS region
-}
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21" # Replace with your desired EKS version
-  subnets         = var.private_subnets
-  vpc_id          = var.vpc_id
-  create_oidc_provider = true
+env_name=$1
 
-  cluster_additional_security_group_ids = [aws_security_group.eks_cluster_sg.id]
+if [ -z "$env_name" ]; then
+  env_name="non-prod"
+fi
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
+for dir in $(ls -d */ | cut -f1 -d '/')
+do
+    echo ""
+    echo "Entering Directory: ${dir}"
+    cd ${dir}
+    
+    echo ""
+    echo "Executing 'terraform init'..."
+    terraform init 
 
-resource "aws_security_group" "eks_cluster_sg" {
-  name_prefix = "eks-cluster-"
-  vpc_id      = var.vpc_id
+    echo ""
+    echo "Executing 'terraform plan -var-file=envs/${env_name}.tfvars -out=${dir}-tfplan'..."
+    terraform plan -var-file=envs/${env_name}.tfvars -out=${dir}-tfplan
 
-  // Define your inbound rules here
-  // Example rule allowing SSH from specified CIDR blocks:
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+    echo ""
+    echo "Executing 'terraform apply -input=false ${dir}-tfplan'..."
+    terraform apply -input=false ${dir}-tfplan 
 
-  // Add other inbound rules as needed
-}
+    rm ${dir}-tfplan
 
-module "eks_fargate_profile_kube_system" {
-  source             = "terraform-aws-modules/eks/aws//modules/fargate"
-  cluster_name       = module.eks.cluster_id
-  fargate_namespace  = "kube-system"
-  pod_execution_role_arn = module.eks.fargate_pod_execution_role_arn
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
+    echo ""
+    echo "Exiting Directory: ${dir}"
+    cd ..
+done 
 
-module "eks_fargate_profile_karpenter" {
-  source             = "terraform-aws-modules/eks/aws//modules/fargate"
-  cluster_name       = module.eks.cluster_id
-  fargate_namespace  = "karpenter"
-  pod_execution_role_arn = module.eks.fargate_pod_execution_role_arn
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-variable "vpc_id" {
-  description = "ID of the VPC where the EKS cluster will be deployed."
-}
-
-variable "private_subnets" {
-  description = "List of private subnets where EKS nodes will be placed."
-  type        = list(string)
-  default     = []
-}
-
-variable "allowed_cidr_blocks" {
-  description = "List of CIDR blocks to allow inbound connections to the EKS cluster security group."
-  type        = list(string)
-  default     = []
-}
